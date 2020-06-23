e704374165e7ce255eab0469d99113951a35a32d
faizalmp <faizalpriyowibowo@gmail.com>
2020-06-24 04:24:53 +0700

    add meta and pre-rendering

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: [ '/' ],
+      })
+    ]
+   }
 }
